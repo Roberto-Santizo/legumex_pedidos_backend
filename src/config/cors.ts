8b10c919f560639ff334780ebad2961d08dd918d
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-04-06 11:04:03 -0600

    feat: cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,6 +4,8 @@ export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const whiteList = [process.env.FRONT_END_URL];
 
-        callback(null, true);
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        }
     }
 }
